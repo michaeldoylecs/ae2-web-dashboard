5ad6405e44d6032a716f154bb322c9c939590ec7
Michael Doyle <mcdoyle7227@gmail.com>
2024-11-24 00:21:27 -0500

    Add basic HTTP endpoint test

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte("Healthy!"))
+	})
+
+	fmt.Println("Server is running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
