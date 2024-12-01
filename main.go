b2d1d3b1f4287900c6085a6aa8e696328fb76a46
Michael Doyle <mcdoyle7227@gmail.com>
2024-12-01 17:49:05 -0500

    Agg log endpoint for testing

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +11,33 @@ func main() {
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
 		w.Write([]byte("Healthy!"))
+	})
+
+	http.HandleFunc("/log", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "/log only accepts POST", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var data map[string]interface{}
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			if err == io.EOF {
+				http.Error(w, "Empty body", http.StatusBadRequest)
+			} else {
+				http.Error(w, "Invalid request body", http.StatusBadRequest)
+			}
+			return
+		}
+
+		message, err := json.Marshal(data)
+		if err != nil {
+			fmt.Println("Failed to Marshal request data")
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Printf("%v\n", string(message))
 	})
 
 	fmt.Println("Server is running at http://localhost:8080")
