9bba758ff85d5f4d46eb42c1b75ff8a8d540d8af
Michael Doyle <mcdoyle7227@gmail.com>
2025-03-23 21:00:00 -0400

    Move ENV check up and also check that env module exists

--- opencomputers/Run.lua
+++ opencomputers/Run.lua
@@ -1,14 +1,15 @@
--- Blank file for test
-local env = require("env")
-local event = require("event")
-local network = require("Network")
-
 -- Check that ENV variables are set
 -- If not, exit early
-if not env.serverUrl or not env.secret then
+local hasEnv, env = pcall(require, "env")
+if not hasEnv or not env.serverUrl or not env.secret then
   print("Please set ENV variables in env.lua")
   os.exit()
 end
+
+--
+
+local event = require("event")
+local network = require("Network")
 
 local function sendStatsToServer()
   network.serverPOST("log", {
